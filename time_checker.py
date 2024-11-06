f58146a45fcc025e2f7fbd821a1ae239a3782d11
sofia alves toreti <sofialvestoreti@gmail.com>
2024-11-05 22:02:10 -0300

    meio caminho andado

--- time_checker.py
+++ time_checker.py
@@ -1,37 +1,73 @@
 import datetime
 import time
 
+
+# gets time in timedelta type
+def get_time(over_midnight: bool):
+    temp: datetime.timedelta
+    if over_midnight:
+        temp = datetime.timedelta(days=1, hours=datetime.datetime.now().hour, minutes=datetime.datetime.now().minute)
+    else:
+        temp = datetime.timedelta(days=0, hours=datetime.datetime.now().hour, minutes=datetime.datetime.now().minute)
+    
+    return temp
+
+
+# defines if process has been up over midnight
+def is_midnight(current: datetime.timedelta, end: datetime.timedelta):
+    end = datetime.timedelta(days=1, hours=8, minutes=00)
+    spooky_hour = datetime.timedelta(hours=0,minutes=0, seconds=0)
+    limit_daytime = end - datetime.timedelta(days=end.days)
+
+    # current = datetime.timedelta(hours=4, minutes=43,seconds=12)
+
+    if spooky_hour < current < limit_daytime:
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
 
-    start = datetime.timedelta(hours=20, minutes=00)
-    end = datetime.timedelta(days=1, hours=8, minutes=00)
-    now = datetime.timedelta(hours=19, minutes=55)
+    start = datetime.timedelta(hours=21, minutes=59)
+    # end = datetime.timedelta(days=1, hours=8, minutes=00)
+    end = datetime.timedelta(hours=22, minutes=00)
 
-    print(now - datetime.timedelta(hours=12))
-
+    print("\n\n\n")
     print(f"start : {start}")
     print(f"end   : {end}")
-    print(f"now   : {now}")
-    print("\n\n\n")
-    print(datetime.datetime.now())
+    print(f"now   : {get_time(False)}")
 
-    current = datetime.timedelta(days=1, hours=datetime.datetime.now().hour, minutes=datetime.datetime.now().minute)
-    print(current)
+    print("\n")
 
-    # Check type and comparison
-    print(current > datetime.timedelta(days=1,hours=16))
+    # MIDNIGHT
+    midnight: bool = is_midnight(get_time(False), end)
+    print(f"midnight: {midnight}")
+
+    current_time = get_time(midnight)
+    print(current_time)
     print("\n\n\n")
 
     num = 0
-    cycle_range = 11
+    cycle_range = 1100
 
     while True:
-        # time.sleep(0.05)
-        now += datetime.timedelta(minutes=1)
+        time.sleep(0.5)
+        now = get_time(midnight)
+        midnight = is_midnight(now, end)
+
+        if  now >= end:
+            print(f"end   : {end}")
+            print(f"now   : {now}")
+            print("close")
+            break
 
         if now >= start:
             while num < cycle_range and now >= start:  # WHILE 2
-                # time.sleep(0.05)
+                if now >= end:
+                    break
+
+                time.sleep(0.5)
                 # if it is not 1/4, you vote
                 if num % 25 != 0:
                     print(f"vote {now} >> {num}")
@@ -46,10 +82,5 @@ if __name__ == "__main__":
                 # increment by one
                 num += 1
                 # end test to see if the time's up
-                now += datetime.timedelta(minutes=1)
-                if now >= end:
-                    break
-
-        if  now >= end:
-            print("close")
-            break
+                now = get_time(midnight)
+                midnight = is_midnight(now, end)
